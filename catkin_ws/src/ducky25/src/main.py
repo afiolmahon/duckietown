e4dda332cefd135d102394d3152674322ab554a1
Antonio Fiol-Mahon <afiolmahon@gmail.com>
2018-12-16 12:28:17 -0500

    test code

--- catkin_ws/src/ducky25/src/main.py
+++ catkin_ws/src/ducky25/src/main.py
@@ -72,9 +72,11 @@ class DuckyNode(object):
         self.ducky_bot.io.at_intersection = self.is_at_intersection
         # get to intersection to begin state machine
         if not self.initial_calibrate:
-            if self.ducky_bot.io.drive_intersection(-1):
-                self.ducky_bot.io.openLoopTurn(0)
-                self.initial_calibrate = True
+            self.ducky_bot.io.openLoopTurn(0)
+            self.initial_calibrate = True
+            #if self.ducky_bot.io.drive_intersection(-1):
+            #    self.ducky_bot.io.openLoopTurn(0)
+            #    self.initial_calibrate = True
         else:
             # self.ducky_bot.state_machine()
             pass
